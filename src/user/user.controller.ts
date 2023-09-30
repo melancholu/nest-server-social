ce6b8a6a56a2cd8fd43b12d29ca1239845af861d
aiden <aiden@safuventures.com>
2023-09-30 22:31:14 +0900

    fix: change user me api order

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -37,6 +37,18 @@ export class UserController {
   }
 
   @UseGuards(AuthGuard())
+  @Get('/me')
+  async getMe(@Request() req): Promise<User> {
+    try {
+      const { uuid } = req.user;
+
+      return await this.userService.getOneByUuid(uuid);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @UseGuards(AuthGuard())
   @Get('/:uuid')
   async getOne(@Param('uuid') uuid: string): Promise<User> {
     try {
@@ -52,18 +64,6 @@ export class UserController {
         throw error;
       }
 
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
-
-  @UseGuards(AuthGuard())
-  @Get('/me')
-  async getMe(@Request() req): Promise<User> {
-    try {
-      const { uuid } = req.user;
-
-      return await this.userService.getOneByUuid(uuid);
-    } catch (error) {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
